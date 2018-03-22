e39312da3b7066920c00a2c3d66989ee6361045c
ALWIN JOHNS <alwinjohns@icloud.com>
2018-03-21 23:22:20 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -6,13 +6,15 @@ const db = require('./config/db')
 const app = express()
 const port = 8000
 
-app.use(bodyParser.urlencoded({ entended: true }))
-
+app.use(bodyParser.urlencoded({ extended: true }))
 
 MongoClient.connect(db.url, (err, database) => {
     if(err) return console.log(err);
     require('./app/routes')(app, database)
     app.listen(port, () => {
-        console.log('hello world', port)
+        console.log('Hello world! ', port)
     })
 })
+//
+// moment("3/14/2018", 'L').fromNow()
+// moment("7/7/2020").format('L')
